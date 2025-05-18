bd1cf32a59ca1a0327c5538de87169519d0c664a
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-05-18 18:22:10 +0700

    First Testing Admin Footer

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh -->
-</div>
+<footer class="bg-white shadow-sm dark:bg-gray-900 absolute bottom-0 left-0 right-0">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">©
+            <script>
+                document.write(new Date().getFullYear())
+            </script> <a href="https://flowbite.com/" class="hover:underline">{{ config('app.name') }}</a>.
+        </span>
+    </div>
+</footer>
