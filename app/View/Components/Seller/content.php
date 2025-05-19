337c10132e0a8f8eb138c9d563f83e62dcb96093
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-05-19 18:57:04 +0700

    Fix bugs

--- app/View/Components/Seller/content.php
+++ app/View/Components/Seller/content.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Content extends Component
+class content extends Component
 {
     public function __construct()
     {
