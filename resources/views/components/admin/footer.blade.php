3661af8ec45d224b79e7cf16b6758274219cf3fd
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-05-19 19:23:38 +0700

    Footer use year instead

--- resources/views/components/admin/footer.blade.php
+++ resources/views/components/admin/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <span class="block text-sm text-gray-500 sm:text-center">©
             <script>
-                document.write(new Date().toLocaleString('default', { month: 'long' }));
+                document.write(new Date().getFullYear());
             </script> <a href="https://flowbite.com/" class="hover:underline">{{ config('app.name') }}</a>.
         </span>
     </div>
