d69fe591ec3ffbf48edcb9a485f742242c5a1a61
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-05-20 12:43:26 +0700

    Testttt

--- resources/views/layout/app3.blade.php
+++ resources/views/layout/app3.blade.php
@@ -33,7 +33,6 @@
             @yield('content')
         </x-admin.content>
         <x-admin.footer></x-admin.footer>
-        <div><div></div></div>njsanasnfkja
     </div>
 </body>
 @vite('resources/js/app.js')
