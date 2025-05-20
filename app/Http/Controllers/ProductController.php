21c56a1f07705d9257860f1fe989ca665e2dbeda
codebetterthanyesterday <fathinabrar.a@gmail.com>
2025-05-20 09:14:06 +0700

    Define show selected product method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index() {
+    protected $productModel;
+    public function __construct()
+    {
+        $this->productModel = Product::class;
+    }
+    public function show($id)
+    {
+        $this->productModel::findOrFail($id);
+        return view('products.show', compact('product'));
+    }
 
-    }
-    public function show($slug) {
-        
-    }
+
 }
